3f84c57b90432e48e1d8e76f33593e730a551896
giappi <giappaig@gmail.com>
2017-06-24 21:39:56 +0700

    + init MatrixCpp

---
+++ com/giappi/lang/Matrix.h
@@ -0,0 +1,54 @@
+/*
+* File:   Matrix.h
+* Author: Giappi
+*
+* Created on 24/06/2017, 18:00 PM
+*/
+
+#pragma once
+#include <string>
+#include <initializer_list>
+
+typedef unsigned int ui32;
+#define null nullptr;
+
+template<class T>
+class Matrix
+{
+public:
+	Matrix(ui32 rows, ui32 cols);
+	/* init matrix with two dimensions array list */
+	Matrix(std::initializer_list<std::initializer_list<T>> list);
+	/* copy constructor */
+	Matrix(Matrix<T>& matrix);
+	~Matrix();
+public:
+	/* get element at @row_index and @col_index */
+	T getElementAt(ui32 row_index, ui32 col_index);
+
+	/* get element at @row_index and @col_index */
+	void setElementAt(ui32 row_index, ui32 col_index, T element);
+
+	/* to get and set element by operator [] */
+	const T* operator [](ui32 index) const;
+	T* operator [](ui32 index);
+
+	/* get numbers of rows */
+	ui32 getWidth();
+
+	/* get numbers of columns */
+	ui32 getHeight();
+
+	/* get matrix as string that is printable */
+	std::string toString();
+
+	/* return a transpose matrix of this matrix */
+	Matrix<T> toTranspose();
+
+private:
+	T** m_matrix = null;
+	ui32 m_rows = 0;
+	ui32 m_cols = 0;
+
+};
+
